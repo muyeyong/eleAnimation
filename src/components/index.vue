a5264011e1127d0c74bcfcaf7d79f99639533077
muyeyong <xuyongshuaige@gmail.com>
2020-12-09 16:08:21 +0800

    1

--- src/components/index.vue
+++ src/components/index.vue
@@ -6,7 +6,7 @@
     <section class="main">
       <section class="foodMenu" id="foodMenu" ref="foodMenu">
         <ul>
-          <li v-for="(item,index) in foods" :key="index" :class="index=== activeMenuIndex? 'activeMenu':''">
+          <li v-for="(item,index) in foods" :key="index" :class="index=== activeMenuIndex? 'activeMenu':''" @click="handleMenuClick(index)">
             {{item.menuName}}
           </li>
         </ul>
@@ -26,7 +26,7 @@
                 <transition name="fade">
                   <span v-if="food.count">{{food.count}}</span>
                 </transition>
-                <span @click="addCar(item.menuName,foodIndex)">
+                <span @click="addCar(item.menuName,foodIndex,$event)">
                     <svg-icon class="add_icon" icon-class="add" ></svg-icon>
                 </span>
               </section>
@@ -37,12 +37,24 @@
     </section>
     <footer class="footer">
       <section class="car_contain">
-        <span class="count">1</span>
-        <svg-icon class="buyCar_icon"  icon-class="buyCar"></svg-icon>
+        <section :class="{car_icon: true,moveDone:dotMoveDone}" ref="carContain">
+          <span class="count">{{totalCount}}</span>
+          <svg-icon class="buyCar_icon"  icon-class="buyCar"></svg-icon>
+        </section>
       </section>
-      <section class="pay">去结算</section>
+      <section :class="{pay:true,canPay:totalCount>0}">去结算</section>
     </footer>
-   
+    <transition
+      appear
+      @after-appear="dotAfterEnter"
+      @before-appear="dotBeforeEnter"
+      v-for="(v,index) in moveDotList"
+      :key="index"
+    >
+      <span class="move_dot" v-if="v">
+        <svg-icon class="add_icon" icon-class="add" ></svg-icon>
+      </span>
+    </transition>
   </div>
 </template>
 
@@ -59,9 +71,17 @@ export default {
       foodsLisTop: [], //食品大类距离顶部的距离
       foodScroll: null,
       activeMenuIndex: 0,
+      moveDotList: [],
+      elBottom: 0,
+      elLeft: 0,
+      windowHeight: null,
+      dotMoveDone: false,
+      totalCount: 0,
+      menuClickEvent: false
     }
   },
   mounted(){
+    this.windowHeight = window.innerHeight;
     this.getFoodListHeight()
   },
   methods:{
@@ -90,27 +110,71 @@ export default {
       this.foodScroll.on('scroll', pos=>{
         if(!this.$refs.foodMenu) return
         const targetIndex = findLastLessValue(this.foodsLisTop,Math.abs(Math.round(pos.y)))
-        
-        if(targetIndex !== -1){
+        if(targetIndex !== -1 && !this.menuClickEvent){
           this.activeMenuIndex = targetIndex
           const nextMenu = this.$refs.foodMenu.querySelectorAll('.activeMenu')[0]
           menuScroll.scrollToElement(nextMenu, 800, 0, -(menuHeight/2 - 50))
         }
       })
     },
+    handleMenuClick(index){
+      this.menuClickEvent = true
+      this.activeMenuIndex = index
+      this.foodScroll.scrollTo(0,-this.foodsLisTop[index],400)
+      this.foodScroll.on("scrollEnd",()=>{
+        this.menuClickEvent = false
+      })
+    },
     removeCar(fatherName,childIndex){
       let target = this.foods.find(item => item.menuName ===  fatherName)
       if(target){
         let count = target.menuList[childIndex].count
+        this.totalCount = this.totalCount>0? this.totalCount - 1 : 0
         target.menuList[childIndex].count = count>0 ? count-1 : 0
       }
     },
-    addCar(fatherName,childIndex){
+    addCar(fatherName,childIndex,event){
       let target = this.foods.find(item => item.menuName ===  fatherName)
       if(target){
+        this.totalCount += 1
+        this.elLeft = event.target.getBoundingClientRect().left
+        this.elBottom = event.target.getBoundingClientRect().bottom
+        this.moveDotList.push(true)
         let count = target.menuList[childIndex].count
         target.menuList[childIndex].count = count + 1
       }
+    },
+    listenInCart(){
+      if(!this.dotMoveDone){
+        this.dotMoveDone = true
+        this.$refs.carContain.addEventListener('animationend',()=>{
+          this.dotMoveDone = false
+        })
+        this.$refs.carContain.addEventListener('webkitAnimationend',()=>{
+          this.dotMoveDone = false
+        })
+
+      }
+    },
+    dotAfterEnter(el){
+      el.style.transform = `translate3d(0,0,0)`;
+      el.children[0].style.transform = `translate3d(0,0,0)`;
+      el.style.transition = 'transform .55s cubic-bezier(0.3, -0.25, 0.7, -0.15)';
+      el.children[0].style.transition = 'transform .55s linear';
+      // eslint-disable-next-line no-unused-vars
+      this.moveDotList = this.moveDotList.map(item => false);
+      el.children[0].style.opacity = 1;
+      el.children[0].addEventListener('transitionend', () => {
+          this.listenInCart();
+      })
+      el.children[0].addEventListener('webkitAnimationEnd', () => {
+          this.listenInCart();
+      })
+    },
+    dotBeforeEnter(el){
+       el.style.transform = `translate3d(0,${37 + this.elBottom - this.windowHeight}px,0)`;
+       el.children[0].style.transform = `translate3d(${this.elLeft - 30}px,0,0)`;
+       el.children[0].style.opacity = 0;
     }
   }
 }
@@ -123,6 +187,40 @@ export default {
 }
 li{
   list-style: none;
+}
+@keyframes carScale {
+  0%{
+    transform: scale(1);
+  }
+  25%{
+    transform: scale(0.75);
+  }
+  50%{
+    transform: scale(1.1);
+  }
+  75%{
+    transform: scale(0.9);
+  }
+  100%{
+    transform: scale(1);
+  }
+}
+@-webkit-keyframes carScale {
+  0%{
+    transform: scale(1);
+  }
+  25%{
+    transform: scale(0.75);
+  }
+  50%{
+    transform: scale(1.1);
+  }
+  75%{
+    transform: scale(0.9);
+  }
+  100%{
+    transform: scale(1);
+  }
 }
 .wrap{
   width: 100%;
@@ -164,7 +262,7 @@ li{
           padding-left: 0.133333rem;
         }
         .activeMenu{
-          border-left: 0.04rem solid #529bda;
+          border-left: .106667rem solid #529bda;
         }
       }
     }
@@ -215,24 +313,33 @@ li{
     width: 100%;
     height: 1.333333rem;
     background: rgb(61, 61, 63);
+    .moveDone{
+      animation: carScale 0.5s ease-in-out;
+    }
     .car_contain{
       position: relative;
       float: left;
       width: 70%;
       height: 100%;
-      .count{
-        position: absolute;
-        top: 0;
-        left: 0.566667rem;
-        display: block;
-        width: 0.5rem;
-        height: 0.5rem;
-        text-align: center;
-        border-radius: 50%;
-        color: #fff;
-        font-size: 0.4rem;
-        background: #ec5728;
+      .car_icon{
+        display: flex;
+        width: .933333rem;
+        height: .933333rem;
+         .count{
+          position: absolute;
+          top: 0;
+          left: 0.566667rem;
+          display: block;
+          width: 0.5rem;
+          height: 0.5rem;
+          text-align: center;
+          border-radius: 50%;
+          color: #fff;
+          font-size: 0.4rem;
+          background: #ec5728;
+        }
       }
+     
     }
     .pay{
       float: right;
@@ -244,6 +351,18 @@ li{
       color: #fff;
       background: #535356;
     }
+    .canPay{
+        background:#529bda;
+      }
+  }
+  .move_dot{
+        position: fixed;
+        bottom: 30px;
+        left: 30px;
+        .add_icon{
+          width: .266667rem;
+          height: .266667rem;
+        }
   }
 }
 .showReduce-enter-active, .showReduce-leave-active {
@@ -259,4 +378,5 @@ li{
 .fade-enter, .fade-leave-active {
     opacity: 0;
 }
+
 </style>
